2573045c695f5a2ef70af277ccb3c0c2e5b551dc
Myles Moylan <moylanm@oregonstate.edu>
2023-03-01 15:16:36 -0800

    implement journal entry screen

--- lib/screens/journal_entry.dart
+++ lib/screens/journal_entry.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:journal/models/journal_entry.dart';
+import 'package:journal/widgets/journal_scaffold.dart';
 
-class JournalEntry extends StatelessWidget {
-  static const routeName = 'journal_entry';
+class JournalEntryScreen extends StatelessWidget {
 
-  const JournalEntry({super.key});
+  final JournalEntry entry;
+
+  const JournalEntryScreen({super.key, required this.entry});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return journalScaffold(
+      title: entry.formattedDate,
+      body: _journalBody()
+    );
+  }
+
+  Widget _journalBody() {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          Text(entry.title, style: const TextStyle(fontSize: 25)),
+          const SizedBox(height: 10.0),
+          Text(entry.body),
+        ],
+      ),
+    );
   }
 }
