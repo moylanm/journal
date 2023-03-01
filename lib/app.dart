1ccb5653243bf509c567bd0f7d35fce17d2b8644
Myles Moylan <moylanm@oregonstate.edu>
2023-03-01 15:00:47 -0800

    opt not to use routes

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:journal/screens/journal_entry.dart';
 import 'package:journal/screens/journal_entry_list.dart';
-import 'package:journal/screens/new_entry.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class App extends StatelessWidget {
-  static final routes = {
-    JournalEntryList.routeName: (context) => const JournalEntryList(),
-    JournalEntry.routeName: (context) => const JournalEntry(),
-    NewJournalEntry.routeName: (context) => const NewJournalEntry(),
-  };
-
   final SharedPreferences preferences;
 
   const App({super.key, required this.preferences});
@@ -20,7 +12,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Journal',
       theme: preferences.getString('theme') == 'dark' ? ThemeData.dark() : ThemeData.light(),
-      routes: routes,
+      home: const JournalEntryListScreen(),
     );
   }
 }
